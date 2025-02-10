9ca40800e3b7ec0947a6434f2f0010c4bc2f2519
pjh612 <pjh_jn@naver.com>
2025-02-10 11:00:35 +0900

    joy-orchestrator - SagaStateRepository.findById 리턴 타입 Optional로 변경

--- joy-orchestrator/src/main/java/com/joy/joyorchestrator/domain/repository/SagaStateRepository.java
+++ joy-orchestrator/src/main/java/com/joy/joyorchestrator/domain/repository/SagaStateRepository.java
@@ -2,10 +2,11 @@ package com.joy.joyorchestrator.domain.repository;
 
 import com.joy.joyorchestrator.domain.model.SagaState;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface SagaStateRepository {
     SagaState save(SagaState sagaState);
 
-    SagaState findById(UUID sagaId);
+    Optional<SagaState> findById(UUID sagaId);
 }
