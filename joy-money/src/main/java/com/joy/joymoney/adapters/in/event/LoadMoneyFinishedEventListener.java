f8190de029c3d65cb49ebfffc0369a6d0636edcd
pjh612 <pjh_jn@naver.com>
2025-02-12 07:22:00 +0900

    joy-money - 머니 충전 Saga Event Listener, Handler 정의
    - 충전 완료 이벤트
    - 충전 요청 생성 완료 이벤트

---
+++ joy-money/src/main/java/com/joy/joymoney/adapters/in/event/LoadMoneyFinishedEventListener.java
@@ -0,0 +1,39 @@
+package com.joy.joymoney.adapters.in.event;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.joy.joymoney.adapters.in.event.handler.LoadMoneyFinishedEventHandler;
+import com.joy.joymoney.adapters.in.event.payload.FirmBankingFinishedPayload;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaHandler;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class LoadMoneyFinishedEventListener {
+
+    private final Logger logger = LoggerFactory.getLogger(LoadMoneyFinishedEventListener.class);
+    private final LoadMoneyFinishedEventHandler handler;
+
+    @KafkaListener(
+            topics = "${kafka.topic.load-money.inbox.events.name}",
+            groupId = "${spring.kafka.consumer.group-id}",
+            containerFactory = "kafkaListenerContainerFactory"
+    )
+    public void handle(@Header(KafkaHeaders.RECEIVED_KEY) UUID sagaId,
+                       @Header("id") String eventId,
+                       @Header("eventType") String eventType,
+                       @Payload FirmBankingFinishedPayload payloadObject) {
+        logger.info("Kafka message received: key = {}, eventType = {}", sagaId, eventType);
+
+        handler.handle(sagaId, payloadObject);
+    }
+}
