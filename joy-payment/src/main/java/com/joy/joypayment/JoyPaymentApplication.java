877e2517ca176c2dce3069504257b0b4c4b2c9c0
pjh612 <pjh_jn@naver.com>
2025-02-12 08:04:16 +0900

    joy-payment - joy-payment 모듈 생성

---
+++ joy-payment/src/main/java/com/joy/joypayment/JoyPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.joy.joypayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoyPaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JoyPaymentApplication.class, args);
+    }
+
+}
